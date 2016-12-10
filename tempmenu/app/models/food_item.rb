fcc92037f70d3a4a1ce095b78b26c1240f3f4e86
Jamie Maher <jamiemaher@jamies-mbp.dhcp.nd.edu>
2016-12-09 20:18:05 -0500

    make prices be formatted correctly

--- tempmenu/app/models/food_item.rb
+++ tempmenu/app/models/food_item.rb
@@ -1,4 +1,11 @@
 class FoodItem < ApplicationRecord
   # jarp: should be lower case and with underscores. They are defining methods
   belongs_to :eatery
+
+  def full_price
+  	String line = '$'
+  	adjusted_price = '%.2f' % price
+  	line = line + adjusted_price.to_s
+  	return line
+  end
 end
